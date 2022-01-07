f83cba3cf62d174d3a232f8e934bc407e6986ce9
DeepthiRM <96856723+DeepthiRM@users.noreply.github.com>
2022-01-06 23:14:51 -0800

    Add files via upload

---
+++ Victory2.java
@@ -0,0 +1,43 @@
+package Assignments;
+
+public class Victory2 {
+
+    public static void main(String[] args) {
+
+        for(int i=0; i<=21; i++){
+            System.out.print("/");
+        }
+
+        System.out.println("\n || Victory is mine ||");
+
+
+        for (int i=0; i<=21; i++){
+            System.out.print("\\");
+
+        }
+        System.out.println("\n ||Victory is mine||");
+
+
+    
+    for (int i=0; i<=21; i++){
+        System.out.print("\\");
+
+    }
+    System.out.println("\n ||Victory is mine||");
+
+    for(int i=0; i<=21; i++){
+        System.out.print("\\");
+    }
+    System.out.println("\n ||Victory is mine||");
+
+    for(int i=0; i<=21; i++){
+        System.out.print("\\");
+    }
+
+    System.out.println("\n ||Victory is mine||");
+
+    for(int i=0; i<=21; i++){
+        System.out.print("\\");
+    }
+}
+}
