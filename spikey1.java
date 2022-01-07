50e45b65f5d4aa5e539ebc7c2e52c19c54db47af
DeepthiRM <96856723+DeepthiRM@users.noreply.github.com>
2022-01-07 00:35:31 -0800

    Add files via upload

---
+++ spikey1.java
@@ -0,0 +1,14 @@
+package Assignments;
+
+public class spikey1 {
+
+    public static void main(String[] args){
+
+        System.out.println("\\/");
+        System.out.println("\\\\//");
+        System.out.println("\\\\\\///");
+        System.out.println("///\\\\\\");
+        System.out.println("//\\\\");
+        System.out.println("/\\");
+    }
+}
