65748140f85f6999427c45e0e1912c0c773cf51d
DeepthiRM <96856723+DeepthiRM@users.noreply.github.com>
2022-01-06 23:14:30 -0800

    Add files via upload

---
+++ victory.java
@@ -0,0 +1,10 @@
+package Assignments;
+
+public class victory {
+    public static void main(String[] args){
+        System.out.println("////////////////////");
+        System.out.println("|| Victory is mine! ||");
+        System.out.println("\\\\\\\\\\\\\\\\\\\\");
+
+    }
+}
