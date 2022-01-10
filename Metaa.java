27588c4ebfec3138281217718f13d8530c131f1e
DeepthiRM <96856723+DeepthiRM@users.noreply.github.com>
2022-01-09 20:19:50 -0800

    Add files via upload

---
+++ Metaa.java
@@ -0,0 +1,12 @@
+package Assignments;
+
+public class Metaa {
+    public static void main(String[] args){
+        System.out.println("public class hello{");
+        System.out.println(" Public static void main(String[] args){");
+        System.out.println(" System.out.println(\"Hello,world!\");");
+        System.out.println("    }");
+        System.out.println("}");
+
+    }
+}
