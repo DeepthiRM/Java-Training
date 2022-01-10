3b18bfb06ffeb5a97bb3448c10d6950bae17da55
DeepthiRM <96856723+DeepthiRM@users.noreply.github.com>
2022-01-09 21:10:33 -0800

    Add files via upload

---
+++ Egg.java
@@ -0,0 +1,24 @@
+package Assignments;
+import java.util.*;
+public class Egg {
+
+    public static void main(String[] args) {
+        printEgg();
+
+    }
+    public static void printEgg(){
+
+
+        System.out.println("     _________");
+        System.out.println("    /         \\");
+        System.out.println("   /           \\");
+        System.out.println("    -\"-'-\"-'-\"-");
+        System.out.println("   \\           /");
+        System.out.println( "    \\_________/");
+        System.out.println();
+        System.out.println();
+
+    }
+
+
+} 
